cb892957c844b30c913925c70e54b5efbfca29ce
apheino <Petteri.Heino@afconsult.com>
2019-06-06 11:40:39 +0300

    new file:   osa1/anekdootit/.gitignore 	new file:   osa1/anekdootit/README.md 	new file:   osa1/anekdootit/package-lock.json 	new file:   osa1/anekdootit/package.json 	new file:   osa1/anekdootit/public/favicon.ico 	new file:   osa1/anekdootit/public/index.html 	new file:   osa1/anekdootit/public/manifest.json 	new file:   osa1/anekdootit/src/index.css 	new file:   osa1/anekdootit/src/index.js

---
+++ osa1/anekdootit/src/index.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const [highest, setHighest] = useState(0)
+    const [points, setPoints] = useState(new Array(props.anecdotes.length).fill(0))
+    
+    const selectNextAnecdote =() => setSelected(Math.floor(Math.random()*props.anecdotes.length))  
+
+    const voteSelectedAnecdote = () => {
+        const copy = [...points]
+        copy[selected] += 1
+
+        let len = copy.length
+        let maxIndex = 0
+        let max = 0
+        while(len--){
+            if (copy[len]>max){
+                max = copy[len]
+                maxIndex = len
+            }
+        }
+
+        setPoints(copy)
+        setHighest(maxIndex)
+        //console.log('Vote: ', copy)    
+    }
+
+    return (
+        <div>
+            <h1>
+                Anecdote of the day
+            </h1>
+            <div>
+                {props.anecdotes[selected]}
+            </div>
+            <div>
+                has {points[selected]} votes
+            </div>
+            <div>       
+                <Button handleClick={voteSelectedAnecdote} text='vote' />
+                <Button handleClick={selectNextAnecdote} text='next anecdote' />
+            </div>
+            <h1>
+                Anecdote with most votes
+            </h1>
+            <div>
+                {props.anecdotes[highest]}
+                <div>
+                has {points[highest]} votes
+            </div>
+            </div>
+        </div>
+    )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
