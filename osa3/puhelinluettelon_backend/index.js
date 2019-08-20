ee7d51a995a3945735ee15d20f6e54b9b91bd018
apheino <a.petteri.heino@gmail.com>
2019-08-20 10:59:25 +0300

    MongoDB Commit

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
-const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
-
-
+/*
 let persons = [
     {
         name: "Arto Hellas",
@@ -27,7 +28,7 @@ let persons = [
         number: "39-23-6423122",
         id: 4
       }
-]
+] */
 
 app.use(cors())
 app.use(express.static('build'))
@@ -49,29 +50,49 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    //res.json(persons)
+    Person.find({}).then(persons => {
+      res.json(persons.map(person => person.toJSON()))
+    })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+app.get('/api/persons/:id', (req, res, next) => {
+    /*const id = Number(req.params.id)
     const person = persons.find(person => person.id === id)
     if (person){
         res.json(person)
     } else {
         res.status(404).end()
     }
-    
+    */
+    Person.findById(req.params.id)
+      .then(person => {
+        if(person){
+          res.json(person.toJSON())
+        } else {
+          res.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+
   }) 
 
 app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    /*res.send(`<p>Phonebook has info for ${persons.length} people</p>
               <p>${new Date().toDateString()} ${new Date().toTimeString()}</p>`)
+    */
+   
+    Person.find({}).then(persons => {
+      res.send(`<p>Phonebook has info for ${persons.length} people</p>
+              <p>${new Date().toDateString()} ${new Date().toTimeString()}</p>`)
+  })
 })
 
+/*
 const generateId = () => {
     return Math.floor(Math.random() * 10000000)
   }
-
+*/
 app.post('/api/persons', (req, res) => {
     const body = req.body
 
@@ -85,34 +106,76 @@ app.post('/api/persons', (req, res) => {
         error: 'number missing' 
         })
     }
-
+    /*
     if (persons.filter(p => p.name === body.name).length > 0){
         console.log('name must be unique')
         return res.status(400).json({ 
             error: 'name must be unique' 
             })
     }
-
-    const person = {
+    */
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateId(),
-    }
-
+        //id: generateId(),
+    })
+    /*
     persons = persons.concat(person)
     res.json(person)
+    */
+    person.save().then(savedPerson => {
+      res.json(savedPerson.toJSON())
+    })
+  
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+  const person = {
+      name: body.name,
+      number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+    /* const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
   
     res.status(204).end()
+    */
+    Person.findByIdAndRemove(req.params.id)
+      .then(result => {
+        res.status(204).end()
+      })
+      .catch(error => next(error))
   })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+      return res.status(400).send({ error: 'malformatted id' })
+  } 
+  
+  next(error)
+}
+  
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
